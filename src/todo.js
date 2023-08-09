f389ca9c9d8b2604e61df064370ace54d408afc1
Justin Lamb <justinlamb@me.com>
2023-08-09 13:25:15 -0700

    todo class

---
+++ src/todo.js
@@ -0,0 +1,9 @@
+class todo {
+  constructor(title, dueDate, priority, notes, list) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.list = list;
+  }
+}
