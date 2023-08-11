658a5e859bc4599dda9f784224206b8fa04b27c5
Justin Lamb <justinlamb@me.com>
2023-08-11 11:49:15 -0700

    clean up

--- src/home.js
+++ src/home.js
@@ -4,18 +4,3 @@ import renderTodoForm from "./todoForm.js";
 renderTodoForm();
 renderTodo();
 
-// document.getElementById('todo-form').addEventListener('submit', function(event) {
-//   event.preventDefault();
-
-//   const title = document.getElementById('title').value;
-//   const dueDate = document.getElementById('due-date').value;
-//   const priority = document.getElementById('priority').value;
-//   const notes = document.getElementById('notes').value;
-//   const list = document.getElementById('list').value;
-
-//   const newTodo = new Todo(title, dueDate, priority, notes, list);
-//   console.log(newTodo); // You can replace this with your actual logic
-
-//   // Reset the form after submission
-//   document.getElementById('todo-form').reset();
-// });
--- src/todoDisplay.js
+++ src/todoDisplay.js
@@ -3,6 +3,8 @@ export default renderTodo;
 function renderTodo(todoItem) {
   const body = document.querySelector("body");
   const todoContainer = document.createElement("div");
+  todoContainer.id = "todo-container";
+  
   const todoTitle = document.createElement("p");
 
   todoTitle.innerHTML = todoItem.title;
