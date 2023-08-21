265c677a17eea9bcb7a040997009828dc56db0a3
Justin Lamb <justinlamb@me.com>
2023-08-21 11:55:04 -0700

    extract checkboxListeners(), use id as key

---
+++ src/checkBox.js
@@ -0,0 +1,23 @@
+export default checkboxListeners;
+
+function markBox(key) {
+  let todoItem = JSON.parse(window.localStorage.getItem(key));
+
+  if (todoItem.completed) {
+    todoItem.completed = false;
+  } else if (!todoItem.completed) {
+    todoItem.completed = true;
+  }
+  window.localStorage.setItem(key, JSON.stringify(todoItem));
+  console.log(todoItem);
+}
+
+function checkboxListeners() {
+  let checkBoxes = document.querySelectorAll("input[type='checkbox']");
+
+  checkBoxes.forEach((checkbox) => {
+    checkbox.onclick = () => {
+      markBox(checkbox.parentElement.id)
+    }
+  })
+}
--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 import renderTodo from "./todoDisplay.js";
 import renderTodoForm from "./todoForm.js";
 import css from "../dist/assets/style.css";
-import markBox from "./markBox.js";
+import checkboxListeners from "./checkBox.js";
 import { deleteListeners } from "./deleteTodo.js";
 import { filterListeners } from "./todoFilter.js";
 
@@ -9,14 +9,6 @@ import { filterListeners } from "./todoFilter.js";
 
 renderTodoForm();
 renderTodo();
+checkboxListeners();
 deleteListeners();
 filterListeners();
-
-let checkBoxes = document.querySelectorAll("input[type='checkbox']");
-
-checkBoxes.forEach((checkbox) => {
-  checkbox.onclick = () => {
-    markBox(checkbox.previousSibling.textContent)
-  }
-})
-
