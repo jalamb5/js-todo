2889381816437b32406e80ff6e69782cab9208be
Justin Lamb <justinlamb@me.com>
2023-08-22 10:38:06 -0700

    implement showModal

--- src/home.js
+++ src/home.js
@@ -4,11 +4,14 @@ import css from "../dist/assets/style.css";
 import checkboxListeners from "./checkBox.js";
 import { deleteListeners } from "./deleteTodo.js";
 import { filterListeners } from "./todoFilter.js";
+import { renderModal, modalListeners } from "./modal.js";
 
 // window.localStorage.clear();
 
 renderTodoForm();
 renderTodo();
+renderModal();
 checkboxListeners();
+modalListeners();
 deleteListeners();
 filterListeners();
--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,22 @@
-export { createModal, modalListener }
+export { renderModal, modalListeners }
 
-function createModal() {
+function renderModal() {
   const todosContainer = document.getElementById("todos-container");
   const modal = document.createElement('dialog');
+  modal.id = "modal";
 
   todosContainer.appendChild(modal);
 }
 
 // Handle buttons outside modal.
-function modalListener() {
-  // add event listeners to view/edit buttons
+function modalListeners() {
+  const modal = document.getElementById("modal");
+  let editButtons = document.querySelectorAll(".edit-btn");
+
+  editButtons.forEach((editButton) => {
+    editButton.onclick = () => {
+      modal.showModal();
+    }
+  })
 }
 
